ef611b16132c539704055847ff166c60f52c1674
Zhymir Thompson <dzhymir@gmail.com>
2025-08-15 15:27:43 -0400

    added init to src subfolder.

---
+++ change_point_algorithms/src/change_point_algorithms/__init__.py
@@ -0,0 +1,11 @@
+# from .change_point_algorithms import *
+#
+# __doc__ = change_point_algorithms.__doc__
+# if hasattr(change_point_algorithms, "__all__"):
+#     __all__ = change_point_algorithms.__all__
+
+from .online_detection import *
+
+__doc__ = online_detection.__doc__
+if hasattr(online_detection, "__all__"):
+    __all__ = online_detection.__all__
