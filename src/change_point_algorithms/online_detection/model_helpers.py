a9bc0de7a08220ce97c87872a87ffbc8cacaeb17
Zhymir Thompson <dzhymir@gmail.com>
2025-08-25 13:23:27 -0400

    Commented out unused functions.

--- src/change_point_algorithms/online_detection/model_helpers.py
+++ src/change_point_algorithms/online_detection/model_helpers.py
@@ -45,49 +45,49 @@ def detection_to_intervals_for_generator_v1_with_progress(
         return detection_to_intervals_for_generator_v1(time_vec, begin, model_generator, start_offset)
 
 
-def detection_to_intervals_for_generator_v2(time_vec, begin, model_generator):
-    """
+# def detection_to_intervals_for_generator_v2(time_vec, begin, model_generator):
+#     """
+#
+#         This version is for detecting when a change has occurred.
+#     """
+#     shock = False
+#     shocks = list()
+#     nonshocks = list()
+#     for idx, is_change in enumerate(model_generator):
+#         if is_change:
+#             if shock:
+#                 shocks.append((time_vec[begin], time_vec[idx]))
+#             else:
+#                 nonshocks.append((time_vec[begin], time_vec[idx]))
+#             begin = idx
+#             shock = not shock
+#     if shock:
+#         shocks.append((time_vec[begin], time_vec[-1]))
+#     else:
+#         nonshocks.append((time_vec[begin], time_vec[-1]))
+#     return shocks, nonshocks
 
-        This version is for detecting when a change has occurred.
-    """
-    shock = False
-    shocks = list()
-    nonshocks = list()
-    for idx, is_change in enumerate(model_generator):
-        if is_change:
-            if shock:
-                shocks.append((time_vec[begin], time_vec[idx]))
-            else:
-                nonshocks.append((time_vec[begin], time_vec[idx]))
-            begin = idx
-            shock = not shock
-    if shock:
-        shocks.append((time_vec[begin], time_vec[-1]))
-    else:
-        nonshocks.append((time_vec[begin], time_vec[-1]))
-    return shocks, nonshocks
 
 
-
-def detection_to_intervals_for_generator_v3(
-        time_vec, begin, model_generator, start_offset=0):
-    """ Convert detections from generator to time intervals
-
-        This version is for detecting when a measure deviates from expected.
-    """
-    shock = False
-    shocks = list()
-    nonshocks = list()
-    for idx, is_change in enumerate(model_generator, start=start_offset):
-        if is_change ^ shock:
-            region = (time_vec[begin], time_vec[idx])
-            shock = not shock
-            begin = idx
-            if is_change:  # change point detected, and not in shock
-                nonshocks.append(region)
-            else:  # no change point detected, and in shock
-                shocks.append(region)
-    if shock:
-        shocks.append((time_vec[begin], time_vec[-1]))
-    else:
-        nonshocks.append((time_vec[begin], time_vec[-1]))
+# def detection_to_intervals_for_generator_v3(
+#         time_vec, begin, model_generator, start_offset=0):
+#     """ Convert detections from generator to time intervals
+#
+#         This version is for detecting when a measure deviates from expected.
+#     """
+#     shock = False
+#     shocks = list()
+#     nonshocks = list()
+#     for idx, is_change in enumerate(model_generator, start=start_offset):
+#         if is_change ^ shock:
+#             region = (time_vec[begin], time_vec[idx])
+#             shock = not shock
+#             begin = idx
+#             if is_change:  # change point detected, and not in shock
+#                 nonshocks.append(region)
+#             else:  # no change point detected, and in shock
+#                 shocks.append(region)
+#     if shock:
+#         shocks.append((time_vec[begin], time_vec[-1]))
+#     else:
+#         nonshocks.append((time_vec[begin], time_vec[-1]))
